09ae3c6fe76eeae06417a0d9b3739e360e8101f9
Slawek Zachcial <slawek.zachcial@gmail.com>
2018-04-30 19:44:25 +0200

    Implement TLS provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
+variable "region" {
+  default = "us-east-2"
+}
+
 provider "aws" {
-  region = "us-east-2"
+  region = "${var.region}"
 }
 
 resource "aws_vpc" "k8s" {
@@ -194,5 +198,170 @@ resource "aws_instance" "worker" {
 
   tags = {
     Name = "worker-${count.index}"
+  }
+}
+
+locals {
+  worker_hostnames     = "${split(",", replace(join(",", aws_instance.worker.*.private_dns), ".${var.region}.compute.internal", ""))}"
+  controller_hostnames = "${split(",", replace(join(",", aws_instance.controller.*.private_dns), ".${var.region}.compute.internal", ""))}"
+}
+
+resource "null_resource" "tls_ca" {
+  provisioner "local-exec" {
+    command = "cfssl gencert -initca tls/ca-csr.json | cfssljson -bare tls/ca"
+  }
+}
+
+resource "null_resource" "tls_admin" {
+  triggers = {
+    tls_ca = "${null_resource.tls_ca.id}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cfssl gencert \
+  -ca=tls/ca.pem -ca-key=tls/ca-key.pem \
+  -config=tls/ca-config.json \
+  -profile=kubernetes \
+  tls/admin-csr.json \
+  | cfssljson -bare tls/admin
+EOF
+  }
+}
+
+data "template_file" "worker_csr" {
+  # Terraform does not allow to use `length` function on computed list
+  # count    = "${length(aws_instance.worker.*.id)}"
+  count = "${length(var.worker_ips)}"
+
+  template = "${file("tls/worker-csr.tpl.json")}"
+
+  vars = {
+    # instance_hostname = "${element(split(".", aws_instance.worker.*.private_dns[count.index]), 0)}"
+    instance_hostname = "${local.worker_hostnames[count.index]}"
+  }
+}
+
+resource "local_file" "worker_csr" {
+  # Terraform does not allow to use `length` function on computed list
+  # count    = "${length(aws_instance.worker.*.id)}"
+  count = "${length(var.worker_ips)}"
+
+  content  = "${data.template_file.worker_csr.*.rendered[count.index]}"
+  filename = "tls/worker-${count.index}-csr.json"
+}
+
+resource "null_resource" "tls_worker" {
+  triggers = {
+    tls_ca = "${null_resource.tls_ca.id}"
+  }
+
+  # Terraform does not allow to use `length` function on computed list
+  # count = "${length(aws_instance.worker.*.id)}"
+  count = "${length(var.worker_ips)}"
+
+  provisioner "local-exec" {
+    command = <<EOF
+cfssl gencert \
+  -ca=tls/ca.pem -ca-key=tls/ca-key.pem \
+  -config=tls/ca-config.json \
+  -hostname=${local.worker_hostnames[count.index]},${aws_instance.worker.*.public_ip[count.index]},${aws_instance.worker.*.private_ip[count.index]} \
+  -profile=kubernetes \
+  ${local_file.worker_csr.*.filename[count.index]} \
+  | cfssljson -bare tls/worker-${count.index}
+EOF
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${tls_private_key.k8s.private_key_pem}"
+  }
+
+  provisioner "file" {
+    source      = "tls/ca.pem"
+    destination = "/home/ubuntu/ca.pem"
+  }
+
+  provisioner "file" {
+    source      = "tls/worker-${count.index}-key.pem"
+    destination = "worker-${count.index}-key.pem"
+  }
+
+  provisioner "file" {
+    source      = "tls/worker-${count.index}.pem"
+    destination = "worker-${count.index}.pem"
+  }
+}
+
+resource "null_resource" "tls_kube_proxy" {
+  triggers = {
+    tls_ca = "${null_resource.tls_ca.id}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cfssl gencert \
+  -ca=tls/ca.pem -ca-key=tls/ca-key.pem \
+  -config=tls/ca-config.json \
+  -profile=kubernetes \
+  tls/kube-proxy-csr.json \
+  | cfssljson -bare tls/kube-proxy
+EOF
+  }
+}
+
+resource "null_resource" "tls_kubernetes" {
+  triggers = {
+    tls_ca = "${null_resource.tls_ca.id}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cfssl gencert \
+  -ca=tls/ca.pem -ca-key=tls/ca-key.pem \
+  -config=tls/ca-config.json \
+  -hostname=10.32.0.1,${join(",", aws_instance.controller.*.private_ip)},${join(",", local.controller_hostnames)},${aws_lb.k8s.dns_name},127.0.0.1,kubernetes.default \
+  -profile=kubernetes \
+  tls/kubernetes-csr.json \
+  | cfssljson -bare tls/kubernetes
+EOF
+  }
+}
+
+resource "null_resource" "tls_controller" {
+  triggers = {
+    tls_ca         = "${null_resource.tls_ca.id}"
+    tls_kubernetes = "${null_resource.tls_kubernetes.id}"
+  }
+
+  # Terraform does not allow to use `length` function on computed list
+  # count = "${length(aws_instance.controller.*.id)}"
+  count = "${length(var.controller_ips)}"
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${tls_private_key.k8s.private_key_pem}"
+  }
+
+  provisioner "file" {
+    source      = "tls/ca.pem"
+    destination = "/home/ubuntu/ca.pem"
+  }
+
+  provisioner "file" {
+    source      = "tls/ca-key.pem"
+    destination = "/home/ubuntu/ca-key.pem"
+  }
+
+  provisioner "file" {
+    source      = "tls/kubernetes-key.pem"
+    destination = "/home/ubuntu/kubernetes-key.pem"
+  }
+
+  provisioner "file" {
+    source      = "tls/kubernetes.pem"
+    destination = "/home/ubuntu/kubernetes.pem"
   }
 }
